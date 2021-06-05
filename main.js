d54ee6cabc95567ce8e2bae626f23db6ecd2722c
Guzel Kisselev <guzelkisselev@gmail.com>
2021-06-04 21:57:29 -0400

    port fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = require("./server");
 const port = process.env.PORT;
 
 const init = async () => {
-  app.listen(PORT, () => console.log(`server flyin on port ${port}`));
+  app.listen(port, () => console.log(`server flyin on port ${port}`));
 };
 
 init();
