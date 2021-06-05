b1ddce81d04bb02ebd6f90a4b6d05d98c4d6a813
Guzel Kisselev <guzelkisselev@gmail.com>
2021-06-04 21:01:22 -0400

    association moved to index file

--- seed.js
+++ seed.js
@@ -1,43 +1,45 @@
 const faker = require("faker");
-const db = require("./server/db/db.js");
-const { Students } = require("./server/db/models/students");
-const { Classes } = require("./server/db/models/classes");
+const { db } = require("./server/db/db");
+const { Students } = require("./server/db/models");
+const { Classes } = require("./server/db/models");
 
 const models = require("./server/db/models");
-const { Student_Classes } = require("./server/db/models/student_classes.js");
+const { Student_Classes } = require("./server/db/models");
 const { seedStudentClasses } = require("./server/scripts/student_classes.js");
-const { Professors } = require("./server/db/models/professors.js");
-const { seedProfessors } = require("./server/scripts/professors.js");
+// const { Professors } = require("./server/db/models/professors.js");
+// const { seedProfessors } = require("./server/scripts/professors.js");
 const { seedClasses } = require("./server/scripts/classes.js");
-const { Wands } = require("./server/db/models/wands.js");
-const { seedWands } = require("./server/scripts/wands");
+// const { Wands } = require("./server/db/models/wands.js");
+// const { seedWands } = require("./server/scripts/wands");
 
-Students.belongsToMany(Classes, {
-  through: Student_Classes,
-  // uniqueKey: "my_custom_unique",
-});
-Classes.belongsToMany(Students, {
-  through: Student_Classes,
-  // uniqueKey: "my_custom_unique",
-});
+// Students.belongsToMany(Classes, {
+//   through: Student_Classes,
+//   // uniqueKey: "my_custom_unique",
+// });
+// Classes.belongsToMany(Students, {
+//   through: Student_Classes,
+//   // uniqueKey: "my_custom_unique",
+// });
 
-// hasMany association will insert foreign key to Classes tables(target)
-Professors.hasMany(Classes);
-// belongsTo association will insert foreign key to Classes(source)
-Classes.belongsTo(Professors);
+// // hasMany association will insert foreign key to Classes tables(target)
+// Professors.hasMany(Classes);
+// // belongsTo association will insert foreign key to Classes(source)
+// Classes.belongsTo(Professors);
 
-Students.hasOne(Wands);
-Wands.belongsTo(Students);
+// Students.hasOne(Wands);
+// Wands.belongsTo(Students);
 
 const seed = async () => {
   try {
+    await db.sync({ force: true });
     // await Professors.sync({ force: true });
     // await seedProfessors();
-    await seedClasses();
-    await seedStudentClasses();
-    await Wands.sync({ force: true });
-    await seedWands(20);
+    // await seedClasses();
+    // await seedStudentClasses();
+    // await Wands.sync({ force: true });
+    // await seedWands(20);
     console.log("seeding success");
+    await db.close();
   } catch (err) {
     console.log("seeding failed", err);
   }
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,1 +1,32 @@
+const { Classes } = require("./classes");
+const { Professors } = require("./professors");
+const { Student_Classes } = require("./student_classes");
+const { Students } = require("./students");
+const { Wands } = require("./wands");
+const { db } = require("../db");
 
+Students.belongsToMany(Classes, {
+  through: Student_Classes,
+  // uniqueKey: "my_custom_unique",
+});
+Classes.belongsToMany(Students, {
+  through: Student_Classes,
+  // uniqueKey: "my_custom_unique",
+});
+
+// hasMany association will insert foreign key to Classes tables(target)
+Professors.hasMany(Classes);
+// belongsTo association will insert foreign key to Classes(source)
+Classes.belongsTo(Professors);
+
+Students.hasOne(Wands);
+Wands.belongsTo(Students);
+
+module.exports = {
+  Students,
+  Professors,
+  Classes,
+  Student_Classes,
+  Wands,
+  db,
+};
